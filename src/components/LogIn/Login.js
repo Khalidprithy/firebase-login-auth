f2ea702f560b4337c7363c8d3d9a316cea1a6bf9
Khalidprithy <khalidprithy@gmail.com>
2022-04-07 23:48:45 +0600

    login form added

---
+++ src/components/LogIn/Login.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import './Login.css'
+import { AiFillFacebook, AiFillGoogleSquare, AiFillGithub } from 'react-icons/ai';
+
+const Login = () => {
+    return (
+        <div className="container">
+            <div className="row d-flex justify-content-center mt-5">
+                <div className="col-12 col-md-8 col-lg-6 col-xl-5">
+                    <div className="card py-3 px-2">
+                        <p className="text-center mb-3 mt-2"> <span className='span-text'> Log In with</span></p>
+                        <div className="row mx-auto ">
+                            <div className="col-4"> <AiFillFacebook className='icons facebook-icon' ></AiFillFacebook> </div>
+                            <div className="col-4"> <AiFillGoogleSquare className='icons google-icon'></AiFillGoogleSquare> </div>
+                            <div className="col-4"> <AiFillGithub className='icons github-icon'></AiFillGithub>  </div>
+                        </div>
+                        <div className="division">
+                            <div className="row">
+                                <div className="col-3">
+                                </div>
+                                <div className="col-6"><span className='span-text'>Login with Email</span></div>
+                            </div>
+                        </div>
+                        <form className="myform">
+                            <div className="form-group"> <input type="email" className="form-control" placeholder="Email" /> </div>
+                            <div className="form-group"> <input type="password" className="form-control" placeholder="Password" /> </div>
+                            <div className="row">
+                                <div className="col-md-6 col-12">
+                                    <div className="form-group form-check"> <input type="checkbox" className="form-check-input" id="exampleCheck1" /> <label className="form-check-label" for="exampleCheck1"> <span className='span-text'>ALready Registered?</span></label> </div>
+                                </div>
+                                <div className="col-md-6 col-12 bn"> <span className='span-text'>Forgot password?</span></div>
+                            </div>
+                            <div className="form-group mt-3"> <button type="button" className="btn btn-block btn-success btn-lg"><small><i className="far fa-user pr-2"></i>Login</small></button> </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
---
+++ src/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDEe37AZlHeoHgytCZeYUIRbQiG1bzx2PY",
+    authDomain: "fir-login-react-1d78c.firebaseapp.com",
+    projectId: "fir-login-react-1d78c",
+    storageBucket: "fir-login-react-1d78c.appspot.com",
+    messagingSenderId: "837089716747",
+    appId: "1:837089716747:web:86fe0b067484c20184b50e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
