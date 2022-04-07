07aedd50a2ebe4f7239eb42835f2fd1484c3ac82
Khalidprithy <khalidprithy@gmail.com>
2022-04-08 02:23:24 +0600

    sign out added

--- src/components/LogIn/Login.js
+++ src/components/LogIn/Login.js
@@ -1,19 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css'
 import { AiFillFacebook, AiFillGoogleSquare, AiFillGithub } from 'react-icons/ai';
 import app from '../../firebase.init';
-import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { getAuth, GithubAuthProvider, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
 
 
 const auth = getAuth(app)
 
 const Login = () => {
-    const provider = new GoogleAuthProvider();
+    const [user, setUser] = useState({});
+
+
+
+    const googleProvider = new GoogleAuthProvider();
+    const githubProvider = new GithubAuthProvider();
+
+    const handleGithubSignIn = () => {
+        console.log('Git')
+    }
+
 
     const handleGoogleSignIn = () => {
-        signInWithPopup(auth, provider)
+        signInWithPopup(auth, googleProvider)
             .then(result => {
                 const user = result.user;
+                setUser(user);
                 console.log(user)
             })
             .catch(error => {
@@ -21,6 +32,18 @@ const Login = () => {
             })
         console.log('working')
     }
+
+    const handleSignOut = () => {
+        signOut(auth)
+            .then(() => {
+                setUser({});
+            })
+            .catch(error => {
+                setUser({});
+            })
+    }
+
+
     return (
         <div className="container">
             <div className="row d-flex justify-content-center mt-5">
@@ -30,7 +53,7 @@ const Login = () => {
                         <div className="row mx-auto ">
                             <div className="col-4"> <AiFillFacebook className='icons facebook-icon' ></AiFillFacebook> </div>
                             <div className="col-4"> <AiFillGoogleSquare onClick={handleGoogleSignIn} className='icons google-icon'></AiFillGoogleSquare> </div>
-                            <div className="col-4"> <AiFillGithub className='icons github-icon'></AiFillGithub>  </div>
+                            <div className="col-4"> <AiFillGithub onClick={handleGithubSignIn} className='icons github-icon'></AiFillGithub>  </div>
                         </div>
                         <div className="division">
                             <div className="row">
@@ -44,14 +67,27 @@ const Login = () => {
                             <div className="form-group"> <input type="password" className="form-control" placeholder="Password" /> </div>
                             <div className="row">
                                 <div className="col-md-6 col-12">
-                                    <div className="form-group form-check"> <input type="checkbox" className="form-check-input" id="exampleCheck1" /> <label className="form-check-label" for="exampleCheck1"> <span className='span-text'>ALready Registered?</span></label> </div>
+                                    <div className="form-group form-check"> <input type="checkbox" className="form-check-input" id="exampleCheck1" /> <label className="form-check-label" for="exampleCheck1"> <span className='span-text'>Already Registered?</span></label> </div>
                                 </div>
                                 <div className="col-md-6 col-12 bn"> <span className='span-text'>Forgot password?</span></div>
                             </div>
-                            <div className="form-group mt-3"> <button type="button" className="btn btn-block btn-success btn-lg"><small><i className="far fa-user pr-2"></i>Login</small></button> </div>
+                            <div className="form-group mt-3">
+                                {
+                                    !user.uid ?
+                                        <button type="button" className="btn btn-block btn-success btn-lg"><small><i className="far fa-user pr-2"></i>Login</small></button>
+                                        :
+                                        <button onClick={handleSignOut} type="button" className="btn btn-block btn-success btn-lg"><small><i className="far fa-user pr-2"></i>Sign Out</small></button>
+                                }
+                            </div>
                         </form>
                     </div>
                 </div>
+            </div>
+
+            <div>
+                <h4>Name: {user.displayName}</h4>
+                <p>Email:{user.email}</p>
+                <img src={user.photoURL} alt="" />
             </div>
         </div>
     );
